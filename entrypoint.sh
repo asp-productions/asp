821a2c4c012c5bd859edb7c9f8c38361721e1b2d
Actor Shop <asp.productions.uk@gmail.com>
2020-01-10 22:00:51 +0000

    Update from Forestry.io - Updated Forestry configuration
    c1
    
    New image
    
    Add new image to build

---
+++ entrypoint.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+set -e
+
+echo $HOME
+echo $GITHUB_WORKSPACE
+echo $GITHUB_REPOSITORY
+
+
+if [ -z "$AWS_S3_BUCKET" ]; then
+  echo "AWS_S3_BUCKET is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
+  exit 1
+fi
+
+# Default to us-east-1 if AWS_REGION not set.
+if [ -z "$AWS_REGION" ]; then
+  AWS_REGION="us-east-1"
+fi
+
+# Default to /public dir if not set.
+if [ -z "$SOURCE_DIR" ]; then
+  SOURCE_DIR="/public"
+fi
+
+# Override default AWS endpoint if user sets AWS_S3_ENDPOINT.
+if [ -n "$AWS_S3_ENDPOINT" ]; then
+  ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
+fi
+
+# Create a dedicated profile for this action to avoid conflicts
+# with past/future actions.
+# https://github.com/jakejarvis/s3-hugo-sync-action/issues/1
+aws configure --profile s3-hugo-sync-action <<-EOF > /dev/null 2>&1
+${AWS_ACCESS_KEY_ID}
+${AWS_SECRET_ACCESS_KEY}
+${AWS_REGION}
+text
+EOF
+
+# Sync using our dedicated profile and suppress verbose messages.
+# All other flags are optional via the `args:` directive.
+# Looks in the container source directory (default /public) and syncs to s3
+sh -c "aws s3 sync ${SOURCE_DIR} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
+              --profile s3-hugo-sync-action \
+              --no-progress \
+              --exclude "${EXCLUDE_FOLDER}" \
+              ${ENDPOINT_APPEND} $*"
+
+# Clear out credentials after we're done.
+# We need to re-run `aws configure` with bogus input instead of
+# deleting ~/.aws in case there are other credentials living there.
+# https://forums.aws.amazon.com/thread.jspa?threadID=148833
+aws configure --profile s3-hugo-sync-action <<-EOF > /dev/null 2>&1
+null
+null
+null
+text
+EOF
